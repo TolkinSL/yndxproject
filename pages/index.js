d2a964bfeb6d848e4e0a464438f5e845e4144f0e
Annebula77 <goharikv@gmail.com>
2022-11-03 23:45:19 +0300

    fix: spell-check in  header and footer

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-const menuBurger = document.querySelector(".header__burger-menue");
-const menuMain = document.querySelector(".header__menue-сontainer");
+const menuBurger = document.querySelector(".header__burger-menu");
+const menuMain = document.querySelector(".header__menu-сontainer");
 const menuAbout = document.querySelector("#fundAbout");
-const menuePrograms = document.querySelector(".header_programs");
-const fundPrograms = document.querySelector("#programsMenue");
+const menuPrograms = document.querySelector(".header_programs");
+const fundPrograms = document.querySelector("#programsMenu");
 const aboutFund = document.querySelector("#fund");
 const buttonClose = document.querySelector(".header__button-close");
 const buttonArrow = document.querySelector(".header__button-arrow");
@@ -10,14 +10,14 @@ const content = document.querySelector(".content");
 
 if (menuBurger) {
   menuBurger.addEventListener("click", function (e) {
-    menuMain.classList.add("header__burger-menue_active");
+    menuMain.classList.add("header__burger-menu_active");
     buttonClose.classList.add("header__buttons_active");
     content.classList.add("content_none");
   });
 }
 
 function closeMenuBurger(menuBurger) {
-  menuBurger.classList.remove("header__burger-menue_active");
+  menuBurger.classList.remove("header__burger-menu_active");
   buttonClose.classList.remove("header__buttons_active");
   content.classList.remove("content_none");
 }
@@ -28,37 +28,37 @@ buttonClose.addEventListener("click", function (e) {
 
 if (menuAbout) {
   aboutFund.addEventListener("click", function(e) {
-    menuAbout.classList.add("header__burger-menue_active");
+    menuAbout.classList.add("header__burger-menu_active");
     buttonArrow.classList.add("header__buttons_active");
   });
 }
 //@ts-check
 
-function closeMenueAbout(menuAbout) {
-  menuAbout.classList.remove("header__burger-menue_active");
+function closeMenuAbout(menuAbout) {
+  menuAbout.classList.remove("header__burger-menu_active");
   buttonArrow.classList.remove("header__buttons_active");
-  menuMain.classList.add("header__burger-menue_active");
+  menuMain.classList.add("header__burger-menu_active");
   buttonClose.classList.add("header__buttons_active");
 }
 buttonArrow.addEventListener("click", function (e) {
-  closeMenueAbout(menuAbout);
+  closeMenuAbout(menuAbout);
 });
 
-if (menuePrograms) {
+if (menuPrograms) {
   fundPrograms.addEventListener("click", function(e) {
-    menuePrograms.classList.add("header__burger-menue_active");
+    menuPrograms.classList.add("header__burger-menu_active");
     buttonArrow.classList.add("header__buttons_active");
   });
 
-  function closeMenuePrograms(menuePrograms) {
-    menuePrograms.classList.remove("header__burger-menue_active");
+  function closeMenuPrograms(menuPrograms) {
+    menuPrograms.classList.remove("header__burger-menu_active");
     buttonArrow.classList.remove("header__buttons_active");
-    menuMain.classList.add("header__burger-menue_active");
+    menuMain.classList.add("header__burger-menu_active");
     buttonClose.classList.add("header__buttons_active");
   }
 
   buttonArrow.addEventListener("click", function (e) {
-    closeMenuePrograms(menuePrograms);
+    closeMenuPrograms(menuPrograms);
   });
 
 }
